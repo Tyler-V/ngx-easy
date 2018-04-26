edad1b1173e179315352374d53d7d37fc525d573
Tyler Vorpahl <tyler.vorpahl@gmail.com>
2018-04-26 17:25:25 -0400

    Reset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ToastrModule } from 'ngx-toastr';
-
-import { MaterialModule } from './modules/material/material.module';
-import { RoutingModule } from './modules/routing/routing.module';
-import { LoginModule } from './modules/login/login.module';
-
-import { AppService } from './app.service';
-import { HttpInterceptorService } from './shared/services/http-interceptor.service';
-
-import { httpInterceptorProviders } from './shared/providers/http-interceptor.providers';
-import { ngxToastrConfig } from './shared/providers/ngx-toastr.config';
 
 import { AppComponent } from './app.component';
-
-const config: any = {
-  'progressBar': true
-};
+import { EasyVirtualScrollModule } from '@ngx-easy/virtual-scroll';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    RoutingModule,
-    LoginModule,
-    ToastrModule.forRoot(ngxToastrConfig)
-  ],
   declarations: [
     AppComponent
   ],
-  providers: [
-    AppService,
-    httpInterceptorProviders
+  imports: [
+    BrowserModule,
+    EasyVirtualScrollModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/providers/http-interceptor.providers.ts
+++
@@ -1,7 +0,0 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-
-import { HttpInterceptorService } from '../services/http-interceptor.service';
-
-export const httpInterceptorProviders = [
-    { provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorService, multi: true },
-];
